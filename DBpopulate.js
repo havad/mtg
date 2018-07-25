ee247a3a9eceb6d539bf823096a355bc5337ca03
havad <yeahprobably@gmail.com>
2018-07-24 19:42:52 -0500

    DBpopulate.js will poplulate an empty db with every magic card from the AllCards-x.json file, had lots of trouble checking to see if card was already added first and this functionality still isn't added

--- DBpopulate.js
+++ DBpopulate.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-var contents = fs.readFileSync("C:\\Users\\chris\\Desktop\\allmtg-cards\\test-cards.json");
+var contents = fs.readFileSync("C:\\Users\\chris\\Desktop\\allmtg-cards\\AllCards-x.json");
 
 var jsonContent = JSON.parse(contents);
 
@@ -8,55 +8,40 @@ var jsonContent = JSON.parse(contents);
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
+//DONE  //TODO: be able to add a full record into the db "cardDB" in "card" collection
+//TODO: check if a record exists so as to not add it twice(having real issues here, i think i'm missing something)
+
 MongoClient.connect(url, {useNewUrlParser: true}, function(err, db) {
     if(err) throw err;
     var dbo = db.db("cardDB");
-    var card = {name: jsonContent["Adorable Kitten"].name, layout: jsonContent["Adorable Kitten"].layout};
-    dbo.collection("cards").insertOne(card, function(err, res) {
-        if (err) throw err;
-        console.log("object inserted into db");
-    });
-    dbo.collection("cards").findOne({}, function(err, res) {
-        if(err) throw err;
-        console.log(res.name + " : " + res.layout);
-    })
-    db.close();
+    dbo.createCollection("cards");
+
+    var count = 0;
+
+    for(item in jsonContent) {
+        dbo.collection("cards").insertOne({
+            name: jsonContent[item].name,
+            layout: jsonContent[item].layout,
+            manaCost: jsonContent[item].manaCost,
+            cmc: jsonContent[item].cmc,
+            colors: jsonContent[item].colors,
+            type: jsonContent[item].type,
+            types: jsonContent[item].types,
+            subtypes: jsonContent[item].subtypes,
+            text: jsonContent[item].text,
+            power: jsonContent[item].power,
+            toughness: jsonContent[item].toughness,
+            imageName: jsonContent[item].imageName,
+            rulings: jsonContent[item].rulings,
+            printings: jsonContent[item].printings,
+            legalities: jsonContent[item].legalities,
+            colorIdentity: jsonContent[item].colorIdentity
+                                });
+        console.log(jsonContent[item].name + " added to DB.");
+        count++;
+    }
+
+    console.log(count + " cards added. DB closing");
+   db.close();
+    
 });
-
-/*var fs = require('fs');
-
-var contents = fs.readFileSync("C:\\Users\\chris\\Desktop\\allmtg-cards\\test-cards.json");
-
-var jsonContent = JSON.parse(contents);
-
-for(item in jsonContent) {
-    console.log(jsonContent[item].layout);
-    console.log(jsonContent[item].name);
-    console.log(jsonContent[item].manaCost);
-    console.log(jsonContent[item].cmc);
-    console.log(jsonContent[item].colors);
-    console.log(jsonContent[item].type);
-    console.log(jsonContent[item].types);
-    console.log(jsonContent[item].subtypes);
-    console.log(jsonContent[item].text);
-    console.log(jsonContent[item].power);
-    console.log(jsonContent[item].toughness);
-    console.log(jsonContent[item].imageName);
-    console.log(jsonContent[item].rulings);
-    console.log(jsonContent[item].printings);
-    console.log(jsonContent[item].legalities);
-    console.log(jsonContent[item].colorIdentity);
-    console.log("");
-}
-
-*/
-/*
-var count = 0;
-
-for (item in jsonContent) {
-    console.log(jsonContent[item].name);
-    count++;
-}
-
-console.log("The number of cards is " + count);
-*/
