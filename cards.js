1764a18fe503bccfa4068acfafcd149e7a70982e
havad <yeahprobably@gmail.com>
2018-07-25 01:36:41 -0500

    added a button and cards.js will find something in the db but not when the button is clicked

--- cards.js
+++ cards.js
@@ -1,3 +1,15 @@
-function go() {
-    console.log("button works!");
-}
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/cardDB", {useNewUrlParser: true}, function (err, db) {
+    if(err) throw err;
+    console.log("Database loaded");
+    var dbo = db.db("cardDB");
+    dbo.createCollection("cards");
+    dbo.collection("cards").find({name: document.getElementById("searchBox").value}).toArray(function(err, res) {
+        if(err) throw err;
+        console.log(res[0].name);
+        
+    });
+    db.close();         
+});
